946a94af91bf2dd95e4caa61a96da454da13a4a1
karan5217 <karan5217@gmail.com>
2020-08-02 18:30:09 +0530

    Updates

---
+++ precache-manifest.ee9e5299531e52ba41c4a2c53e3f1a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adbeb2c5d240ec4b5cb0b4a0b193c91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7370ba382958ec8e835",
+    "url": "/static/css/main.80dac9c0.chunk.css"
+  },
+  {
+    "revision": "3d4140f14fb54bac2aff",
+    "url": "/static/js/2.fe3354bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fe3354bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7370ba382958ec8e835",
+    "url": "/static/js/main.fdfa6fc8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
